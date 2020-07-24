cc5a2580944b5904572a31a55dc6465e61cae328
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-24 00:26:45 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,15 @@
+class MP3Importer
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |i| i.gsub("#{path}/", "") }
+  end
+
+  def import
+    files.each{|i| Song.new_by_filename(i)}
+  end
+end
